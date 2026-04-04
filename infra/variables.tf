102d10be2cc69cd67b00413df8053ff63d8a6fcd
Vu Duc <vuduc112299@gmail.com>
2026-04-04 19:26:20 +0900

    update infra, new host

--- infra/variables.tf
+++ infra/variables.tf
@@ -13,15 +13,10 @@ variable "project_name" {
 variable "instance_type" {
   description = "EC2 instance type (t3.micro = free tier in ap-southeast-1)"
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "key_pair_name" {
   description = "Name of the SSH key pair (create in AWS Console first)"
-  type        = string
-}
-
-variable "my_ip" {
-  description = "Your public IP in CIDR notation (e.g. 113.190.x.x/32). Find with: curl ifconfig.me"
   type        = string
 }
